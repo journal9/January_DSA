be2dfb5197cc73fc2dba761f7a374ed956a50e68
Deepika-J <step.strong.79@gmail.com>
2024-01-08 23:54:35 +0530

    first commit

---
+++ Arrays_JS.js
@@ -0,0 +1,18 @@
+let a=[2,4,6,8,4,3,9,10]
+
+console.log(a.length)
+
+console.log(a.toString())
+console.log(a.at(4))
+
+b=['aa','ac','bs','mn','et']
+let c = b.join('-')
+console.log(c)
+
+let k = b.splice(1,3)
+console.log(k)
+
+let m = b.splice(1,2)
+console.log(m)
+
+console.log(b)
