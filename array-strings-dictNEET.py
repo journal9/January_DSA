29bad54bf8f84a48d7d7fb55b743462fc73911ca
Deepika-J <step.strong.79@gmail.com>
2024-01-17 13:57:15 +0530

    verify soduko board

--- array-strings-dictNEET.py
+++ array-strings-dictNEET.py
@@ -159,4 +159,51 @@ def productExceptSelf(nums: List[int]) -> List[int]:
                 res.append(reduce((lambda x, y: x * y), temp))
         return res
 
-productExceptSelf([1,2,3,4])   
+productExceptSelf([1,2,3,4])
+
+#Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+def isValidSudoku(board: List[List[str]]) -> bool:
+        res=[]
+
+        for i in range(9):
+            for j in range(9):
+                cell=board[i][j]
+
+                if cell != ".":
+                    res+=[(i,cell),(cell,j),(i//3,j//3,cell)]
+
+        
+        return len(res)==len(set(res))
+
+def isValidSudoku(board: List[List[str]]) -> bool:
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        squares = collections.defaultdict(set)  # key = (r /3, c /3)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (
+                    board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r // 3, c // 3)]
+                ):
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+
+        return True
+
+board = [["5","3",".",".","7",".",".",".","."]
+        ,["6",".",".","1","9","5",".",".","."]
+        ,[".","9","8",".",".",".",".","6","."]
+        ,["8",".",".",".","6",".",".",".","3"]
+        ,["4",".",".","8",".","3",".",".","1"]
+        ,["7",".",".",".","2",".",".",".","6"]
+        ,[".","6",".",".",".",".","2","8","."]
+        ,[".",".",".","4","1","9",".",".","5"]
+        ,[".",".",".",".","8",".",".","7","9"]]  
+
+isValidSudoku(board)    
