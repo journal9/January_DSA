fa5eebf8e8ff07711270bc3c23c17a703820ed0a
Deepika-J <step.strong.79@gmail.com>
2024-02-16 12:16:59 +0530

    container with most water

--- array-strings-dictNEET.py
+++ array-strings-dictNEET.py
@@ -393,3 +393,20 @@ def threeSum(nums: List[int]) -> List[List[int]]:
             else:
                 lp+=1
     return triplets   
+
+
+# You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+# Find two lines that together with the x-axis form a container, such that the container contains the most water.
+def maxArea(height: List[int]) -> int:
+    l = 0
+    r = len(height) - 1
+    maxArea = 0
+    while l<r:
+        maxArea = max(maxArea, (r - l) * min(height[l], height[r]))
+        if height[l] > height[r]:
+            r -= 1
+        else:
+            l += 1
+    return maxArea
+
+maxArea([1,8,6,2,5,4,8,3,7])
--- test.py
+++ test.py
@@ -19,31 +19,64 @@
 #     if o in numbers:
 #         print(i+1,numbers.index(o)+1)    
 
-nums = [-1,0,1,2,-1,-4]
-nums.sort()
-triplets=[]
-for i in range(len(nums)):
-    a = nums[i]
-    lp = i+1 ;rp = len(nums)-1
-    print(nums)
-    print(nums)
-    while lp<rp:
-        print(nums[lp])
-        print(nums[rp])    
-        if nums[lp]+nums[rp]==-a:
-            an = [a,nums[lp],nums[rp]]
-            an.sort()
-            if an not in triplets:
-                triplets.append(an)
-            lp+=1
-            rp-=1
-        elif nums[lp]+nums[rp]>-a:
-            rp-=1
-        else:
-            lp+=1
+# nums = [-1,0,1,2,-1,-4]
+# nums.sort()
+# triplets=[]
+# for i in range(len(nums)):
+#     a = nums[i]
+#     lp = i+1 ;rp = len(nums)-1
+#     print(nums)
+#     print(nums)
+#     while lp<rp:
+#         print(nums[lp])
+#         print(nums[rp])    
+#         if nums[lp]+nums[rp]==-a:
+#             an = [a,nums[lp],nums[rp]]
+#             an.sort()
+#             if an not in triplets:
+#                 triplets.append(an)
+#             lp+=1
+#             rp-=1
+#         elif nums[lp]+nums[rp]>-a:
+#             rp-=1
+#         else:
+#             lp+=1
 
-print(triplets)
-print('no')
+# print(triplets)
+# print('no')
+
+from ast import List
+
+
+height = [1,8,6,2,5,4,8,3,7]
+def maxArea(height):
+    indMax = []
+    for i in range(len(height)-1):
+        max_pdt = 1
+        if height[i]==1:
+            max_pdt = ((len(height)-i-1)*1)
+            print(max_pdt)
+            indMax.append(max_pdt) 
+            continue 
+        for j in range(i+1,len(height)):
+            print(height[i])
+            print(height[j])
+            if (j-i)*min(height[i],height[j])>max_pdt:
+                max_pdt = ((j-i)*min(height[i],height[j]))
+        print(max_pdt)
+        indMax.append(max_pdt)  
+    indMax.sort()
+    print(indMax)
+    print(indMax[-1])         
+
+                
+
+
+
+
+
+
+maxArea(height)
 
 
 
