a7648d505d7a18a44cffb8392fd094f85edc63fe
Deepika-J <step.strong.79@gmail.com>
2024-02-23 01:05:36 +0530

    next permutation

--- 2pointer.py
+++ 2pointer.py
@@ -182,3 +182,18 @@ def fourSum(nums, target):
         return res_ls
 # ret = fourSum([-2,-1,0,0,1,2],0)
 # print(ret)
+
+#A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+#For example, for arr = [1,2,3], the following are all the permutations of arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+def nextPermutation(nums):
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+        if i == -1:
+            nums.reverse()
+            return
+        j = len(nums) - 1
+        while nums[j] <= nums[i]:
+            j -= 1
+        nums[i], nums[j] = nums[j], nums[i]
+        nums[i + 1:] = reversed(nums[i + 1:])
--- test.py
+++ test.py
@@ -94,32 +94,95 @@
 # ret = threeSumClosest([0,1,2],3)
 # print(ret)
 
-def fourSum(nums, target):
-        nums.sort()
-        res_ls=[]
-        for i in range(len(nums)):
-            for m in range(i+1,len(nums)-1):
-                j=m+1
-                k=len(nums)-1
-                while j<k:
-                    print("----------------m")
-                    print(nums[i])
-                    print(nums[m])
-                    print(nums[j])
-                    print(nums[k])
-                    sum = nums[i]+nums[m]+nums[j]+nums[k]
-                    if sum==target:
-                        if [nums[i],nums[m],nums[j],nums[k]] not in res_ls:
-                            res_ls.append([nums[i],nums[m],nums[j],nums[k]])
-                        j+=1
-                    if sum<target:
-                        j+=1
-                    if sum>target:
-                        k-=1
-            if j>k:
-                return res_ls
-        return res_ls
-ret = fourSum([-2,-1,0,0,1,2],0)
+# def fourSum(nums, target):
+#         nums.sort()
+#         res_ls=[]
+#         for i in range(len(nums)):
+#             for m in range(i+1,len(nums)-1):
+#                 j=m+1
+#                 k=len(nums)-1
+#                 while j<k:
+#                     print("----------------m")
+#                     print(nums[i])
+#                     print(nums[m])
+#                     print(nums[j])
+#                     print(nums[k])
+#                     sum = nums[i]+nums[m]+nums[j]+nums[k]
+#                     if sum==target:
+#                         if [nums[i],nums[m],nums[j],nums[k]] not in res_ls:
+#                             res_ls.append([nums[i],nums[m],nums[j],nums[k]])
+#                         j+=1
+#                     if sum<target:
+#                         j+=1
+#                     if sum>target:
+#                         k-=1
+#             if j>k:
+#                 return res_ls
+#         return res_ls
+# ret = fourSum([-2,-1,0,0,1,2],0)
+# print(ret)
+
+def nextPermutation(nums):
+    temp=0
+    for i in range(len(nums)-2):
+        j=i+1
+        k=j+1
+        if nums[j]>=nums[i]:
+            i+=1
+            j+=1
+            continue
+        if nums[i]>nums[j]:
+            if j==len(nums)-2:
+                nums.unshift(nums[-1])
+                nums.pop(-1)
+                return nums
+            if nums[j]<nums[k]:
+                temp=nums[k]
+                nums[k]=nums[j]
+                nums[j]=temp
+                return nums
+            else:
+                temp=nums[k]
+                nums[k]=nums[i]
+                nums[i]=temp
+                return nums  
+
+def nextPermutation2(nums):
+    temp=0
+    for i in range(len(nums)-1,-1,-1):
+        print(nums[i])
+        j=i-1
+        print(nums[j])
+        if nums[j]<nums[i]:
+            temp=nums[j]
+            nums[j]=nums[i]
+            nums[i]=temp
+            return nums
+        else: 
+            while nums[j]>=nums[i]:
+                if j>0:
+                    j-=1 
+                else:
+                    break 
+            #part = list(nums[j+1:i]).sort()
+            part = sorted(nums[j+1:i+1])
+            print(part)
+            a=part[-1]
+            b=nums[j]
+            nums[j]=a
+            part[-1]=b
+            # nums[j] = part[-1]
+            # part[-1]=nums[j]
+            # print(nums[j])
+            nums = nums[:j+1]+part
+            # nums.unshift(nums[j-1])  
+            # nums.pop(j+1)
+            # temp=nums[j]
+            return nums
+    return nums              
+ret = nextPermutation2([
+    3,2,1
+])
 print(ret)
 
 
