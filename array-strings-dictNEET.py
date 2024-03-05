3bdae1f4df92290d04fa6c59a61596a2bf7d0527
Deepika-J <step.strong.79@gmail.com>
2024-03-05 23:01:29 +0530

    changes added

--- array-strings-dictNEET.py
+++ array-strings-dictNEET.py
@@ -307,3 +307,12 @@ def longestConsecutive(nums: List[int]) -> int:
         
         return max_length
 
+def strStr(haystack, needle):
+    if needle==haystack:
+            return 0
+    if len(needle) > len(haystack):
+        return -1
+    for i in range(len(haystack) - len(needle) + 1):
+        if haystack[i:i+len(needle)] == needle:
+            return i
+    return -1
