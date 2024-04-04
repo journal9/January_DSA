e6486d2819e9a9d6ced46bc38ff2d21e8d45c93e
Deepika-J <step.strong.79@gmail.com>
2024-04-04 20:20:45 +0530

    bit manipulation

--- bit_manipulation.py
+++ bit_manipulation.py
@@ -22,4 +22,32 @@ def solve(A, B):
 def solve(A, B):
     if A & (1<<B)>0:
         A=A^(1<<B)
-    return A   
+    return A  
+
+# You are given two integers A and B.
+# Return 1 if B-th bit in A is set
+# Return 0 if B-th bit in A is unset 
+def solve(A, B):
+    if A & (1<<B)>0:
+        return 1
+    if A & (1<<B)==0:
+        return 0
+    
+# Given an integer A. Unset B bits from the right of A in binary.
+def solve(A, B):
+    i=0
+    while i<B:
+        if A & (1<<i) > 0:
+            A = A^(1<<i)
+        i+=1
+    return A
+
+# Given an integer A, find and return the Ath magic number.
+# A magic number is defined as a number that can be expressed as a power of 5 or a sum of unique powers of 5.
+# First few magic numbers are 5, 25, 30(5 + 25), 125, 130(125 + 5), ….
+def solve(A):
+    magic = 0
+    for i in range(0,32):
+        if A& 1<<i >0:
+            magic+=pow(5,i+1)
+    return magic    
